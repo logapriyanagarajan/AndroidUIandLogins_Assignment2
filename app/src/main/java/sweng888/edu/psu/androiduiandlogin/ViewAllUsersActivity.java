4d9048efdbb508ae910220016ae0191118706c9c
logapriyanagarajan <44417186+logapriyanagarajan@users.noreply.github.com>
2018-11-30 12:26:18 -0500

    Update ViewAllUsersActivity.java

--- app/src/main/java/sweng888/edu/psu/androiduiandlogin/ViewAllUsersActivity.java
+++ app/src/main/java/sweng888/edu/psu/androiduiandlogin/ViewAllUsersActivity.java
@@ -3,6 +3,8 @@ package sweng888.edu.psu.androiduiandlogin;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ListView;
 
@@ -14,6 +16,9 @@ import sweng888.edu.psu.androiduiandlogin.model.UserProfile;
 
 public class ViewAllUsersActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    RecyclerAdapter adapter;
+
     private ArrayList<UserProfile> u1;
     private PersistanceUserData persistanceUserData;
 
@@ -21,21 +26,32 @@ public class ViewAllUsersActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.user_listing);
+        setContentView(R.layout.recycler_view);
+
+        u1 = new ArrayList<>();
 
         persistanceUserData = new PersistanceUserData(this);
         u1 = persistanceUserData.getDataFromDB();
 
 
-        ListView list = (ListView) findViewById(R.id.list);
-        list.setAdapter(new UserAdapter(this, loadData()));
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //ListView list = (ListView) findViewById(R.id.list);
+        //list.setAdapter(new UserAdapter(this, loadData()));
+
+        adapter = new RecyclerAdapter(this, u1);
+        recyclerView.setAdapter(adapter);
+
+        //loadData();
+
 
         }
 
-    private ArrayList<UserProfile> loadData() {
+    private void loadData() {
         ArrayList<UserProfile> list = new ArrayList<>();
 
-        UserList u = null;
+        UserProfile u = null;
 
         String name;
         String surname;
@@ -51,7 +67,8 @@ public class ViewAllUsersActivity extends AppCompatActivity {
                 list.add(i);
             }
             }
-            return list;
+            adapter = new RecyclerAdapter(this, list);
+        recyclerView.setAdapter(adapter);
     }
 }
 
